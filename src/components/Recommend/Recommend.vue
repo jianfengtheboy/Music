0cd36ca349e76d3d5d9938b2b4a76bed0807384b
jianfengtheboy <jianfengtheboy@163.com>
2018-05-28 22:42:43 +0800

    renew in 5.28 22:42pm

--- src/components/Recommend/Recommend.vue
+++ src/components/Recommend/Recommend.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script type="text/ecmacript-6">
-import { getRecommend } from 'api/recommend'
+import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 import Slider from '@/base/Slider/Slider'
 
@@ -33,12 +33,20 @@ export default {
     },
     created () {
         this._getRecommend()
+        this._getDiscList()
     },
     methods : {
         _getRecommend() {
             getRecommend().then((res) => {
                 if(res.code === ERR_OK) {
                     this.recommends = res.data.slider
+                }
+            })
+        },
+        _getDiscList() {
+            getDiscList().then((res) => {
+                if(res.code === ERR_OK) {
+                    console.log(res.data.list)
                 }
             })
         }
